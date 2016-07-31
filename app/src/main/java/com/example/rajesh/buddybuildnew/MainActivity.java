0a2897f10689981973f82fc0b6d98d0007909f07
shubham jain <shubhamjain.2303@gmail.com>
2016-07-31 13:01:48 +0530

    trying to crash the assp

--- app/src/main/java/com/example/rajesh/buddybuildnew/MainActivity.java
+++ app/src/main/java/com/example/rajesh/buddybuildnew/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.rajesh.buddybuildnew;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_one= (TextView) findViewById(R.id.tv_one);
+        tv_one.setText(null);
+        if(tv_one.getText().toString().equals("ssh")){
+            Toast.makeText(this,"d",Toast.LENGTH_SHORT).show();
+        }
     }
 }
